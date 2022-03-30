d30fbba71f67c558373dad00f8417ce892c5eb81
Ariel Davis <ariel.z.davis@icloud.com>
2022-03-29 23:31:12 -0700

    Show host and port

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::net::SocketAddr;
 use tokio::runtime::Runtime;
 
 fn main() {
-  println!("starting up");
-  let app = Router::new().route("/", get(root));
   let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 3000));
+  println!("starting up on {:?}", addr);
+  let app = Router::new().route("/", get(root));
   Runtime::new().unwrap().block_on(async {
     Server::bind(&addr)
       .serve(app.into_make_service())
